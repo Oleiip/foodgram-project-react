0c990bd2c0a692e311a144460bdb841f90fd8705
Павел <59635294+Oleiip@users.noreply.github.com>
2023-03-22 10:56:09 +0300

    Update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*'), '*']
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
